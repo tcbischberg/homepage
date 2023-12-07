da856f51cf5d2e37a8b2d5684c2f80af2bdcae34
Kenny <kenny16372@gmail.com>
2023-12-07 22:19:03 +0100

    feat: Start supporting large screens

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  cssWhitespaceSensitivity: 'ignore',
+  jsWhitespaceSensitivity: 'ignore',
+};
